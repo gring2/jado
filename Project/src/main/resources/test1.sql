6b8acc33c0f36b24947eecd262e7a328c231db8e
luckymanx <luckykhkx@gmail.com>
2016-04-12 19:40:29 +0900

    디폴트 값 및 가상데이터들 추가
    해당 파일의 모든 sql문을 통째로 실행하면 됨(전체선택=>alt+x)

--- Project/src/main/resources/test1.sql
+++ Project/src/main/resources/test1.sql
@@ -22,7 +22,7 @@ CREATE TABLE JADO_CHAT
 	content varchar2(4000) NOT NULL,
 	chatType varchar2(10),
 	startDate date,
-	chatDate date NOT NULL,
+	chatDate date DEFAULT sysdate NOT NULL,
 	memNum varchar2(20) NOT NULL,
 	thmNum varchar2(10) NOT NULL,
 	PRIMARY KEY (chatNum)
@@ -52,7 +52,7 @@ CREATE TABLE JADO_MEMBER
 	memNum varchar2(20) NOT NULL,
 	userNum varchar2(10) NOT NULL,
 	proNum varchar2(10) NOT NULL,
-	agreed char NOT NULL,
+	agreed char DEFAULT 'N' NOT NULL,
 	PRIMARY KEY (memNum)
 );
 
@@ -61,7 +61,7 @@ CREATE TABLE JADO_PROJECT
 (
 	proNum varchar2(10) NOT NULL,
 	title varchar2(100) NOT NULL,
-	startDate date NOT NULL,
+	startDate date DEFAULT sysdate NOT NULL,
 	enddate date,
 	PRIMARY KEY (proNum)
 );
@@ -170,9 +170,55 @@ ALTER TABLE JADO_VOTE
 	REFERENCES JADO_USER (userNum)
 ;
 
-CREATE SEQUENCE seq_user INCREMENT BY 1 START WITH 1 MAXVALUE 999999999
+drop sequence seq_user;
+drop sequence seq_pro;
+drop sequence seq_thm;
+drop sequence seq_cht;
 
-CREATE SEQUENCE seq_pro INCREMENT BY 1 START WITH 1 MAXVALUE 999999999
+CREATE SEQUENCE seq_user INCREMENT BY 1 START WITH 1 MAXVALUE 999999999;
 
-CREATE SEQUENCE seq_cht INCREMENT BY 1 START WITH 1 MAXVALUE 9999999999999999999
+CREATE SEQUENCE seq_pro INCREMENT BY 1 START WITH 1 MAXVALUE 999999999;
+
+CREATE SEQUENCE seq_thm INCREMENT BY 1 START WITH 1 MAXVALUE 999999999;
+
+CREATE SEQUENCE seq_cht INCREMENT BY 1 START WITH 1 MAXVALUE 9999999999999999999;
+
+
+insert into jado_user (userNum,id,password,name ,googleId)				
+	values ('U'||to_char(lpad(seq_user.nextval, 9, '0')),'asdf','asdf','asdf','asdf');
+insert into jado_user (userNum,id,password,name ,googleId)				
+	values ('U'||to_char(lpad(seq_user.nextval, 9, '0')),'qwer','qwer','qwer','qwer');
+insert into jado_user (userNum,id,password,name ,googleId)				
+	values ('U'||to_char(lpad(seq_user.nextval, 9, '0')),'zxcv','zxcv','zxcv','zxcv');
+insert into jado_user (userNum,id,password,name ,googleId)				
+	values ('U'||to_char(lpad(seq_user.nextval, 9, '0')),'1234','1234','1234','1234');
+
+insert into jado_project (proNum,title)
+	values ('P'||to_char(lpad(seq_pro.nextval, 9, '0')),'프로젝트1');
+insert into jado_project (proNum,title)
+	values ('P'||to_char(lpad(seq_pro.nextval, 9, '0')),'프로젝트2');
+
+insert into jado_member (userNum,proNum,memNum,agreed)
+	values ('U000000001','P000000001', concat('U000000001','P000000001'),'Y');
+insert into jado_member (userNum,proNum,memNum,agreed)
+	values ('U000000002','P000000001', concat('U000000002','P000000001'),'Y');
+insert into jado_member (userNum,proNum,memNum,agreed)
+	values ('U000000003','P000000001', concat('U000000003','P000000001'),'Y');
+insert into jado_member (userNum,proNum,memNum,agreed)
+	values ('U000000003','P000000002', concat('U000000003','P000000002'),'Y');
+insert into jado_member (userNum,proNum,memNum,agreed)
+	values ('U000000004','P000000002', concat('U000000004','P000000002'),'Y');
+	
+insert into jado_theme (thmNum,proNum,themeName)
+	values ('T'||to_char(lpad(seq_thm.nextval, 9, '0')),'P000000001','채팅1-1');
+insert into jado_theme (thmNum,proNum,themeName)
+	values ('T'||to_char(lpad(seq_thm.nextval, 9, '0')),'P000000001','채팅1-2');
+insert into jado_theme (thmNum,proNum,themeName)
+	values ('T'||to_char(lpad(seq_thm.nextval, 9, '0')),'P000000002','채팅2-1');
+insert into jado_theme (thmNum,proNum,themeName)
+	values ('T'||to_char(lpad(seq_thm.nextval, 9, '0')),'P000000002','채팅2-2');
+
+
+	
+
 
